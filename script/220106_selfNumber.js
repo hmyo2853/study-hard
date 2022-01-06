336f691bf9e84495d2ea31b91f7ced7f2b8fe4e1
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-07 00:00:09 +0900

    Update 220106_selfNumber.js

--- script/220106_selfNumber.js
+++ script/220106_selfNumber.js
@@ -1,28 +1,38 @@
 /* 1. 각 자릿수 합 = 생성자가 있는 양수를 구하는 함수 */
 /* 2. array1에 push하여 생성자가 있는 양수의 배열을 만들기 */
-/* 3. 전체 array1에서 array2의 중복 빼기(셀프넘버 구하기) */
+/* 3. 전체 array1에서 array2의 값 빼기(셀프넘버 구하기) */
+
+let array1 : Array<number> = [];
 let array2 : Array<number> = [];
-let num : number = 1; //1부터 시작
+let n : number = 1;
 let answer : number = 0;
 
-while ( answer < 10003 ) { // 배열에 들어가는 answer값이 10000보다 작거나 같을때까지 반복
-  function solution( num : number ) {
-      let sum : number = 0;
-      let originNum : number = num;
+// array1을 만들어줌 1부터 2,3,4, ..., 10000
+for ( let i = 1; i < 10001; i++ ) {
+  array1.push(i);
+}
 
-      while( num > 0 ) {
-          sum += num % 10;
-          num = Math.floor(num / 10);
+while ( answer < 10003 ) { // 배열에 들어가는 answer값이 10000보다 작거나 같을때까지 반복
+  function d( n : number ) {
+      let sum : number = 0;
+      let originNum : number = n;
+
+      while( n > 0 ) {
+          sum += n % 10;
+          n = Math.floor(n / 10);
       } //0에서 반복문 나감
-      console.log(originNum);
       answer = originNum + sum;
       return answer;
   }
-  array2.push(solution(num));
+  array2.push(d(n));
   array2.sort(function(a,b) { // 오름차순 2,4, ...
     return a - b;
   });
-  num++;
+  n++;
 }
 
-console.log(array2) // 배열값 모두 들어간 것 
+array1 = array1.filter(x => !array2.includes(x));
+
+for ( let j = 0; j < array1.length; j++ ) {
+  console.log(array1[j]);
+}
