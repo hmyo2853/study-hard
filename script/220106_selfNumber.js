ac348f191b078c798b915c86c65406f8f8a45eb3
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-06 16:47:00 +0900

    Update 220106_selfNumber.js

--- script/220106_selfNumber.js
+++ script/220106_selfNumber.js
@@ -1,13 +1,20 @@
-/* 각 자릿수 합 = 생성자가 있는 양수를 구하는 함수 */
-function solution( num : number ) : number {
-  let sum : number = 0;
-  let originNum : number = num;
+/* 1. 각 자릿수 합 = 생성자가 있는 양수를 구하는 함수 */
+/* 2. array1에 push하여 생성자가 있는 양수의 배열을 만들기*/
+/* 3. 전체 array1에서 array2의 중복 빼기(셀프넘버 구하기) */
+let array2 : Array<number> = [];
 
-  while(num > 0) { //256 25 2, 0에서 반복문 나감
-    sum += num % 10; //0 + 6 6 + 5 13 + 0 sum = 13
-    num = Math.floor(num / 10); // num = 25 2 0
-  }
-  let answer : number = originNum + sum  //256 + 13
-  return answer; 
+for (let num = 257; num < 261; ++num) { //257부터 261까지
+    function solution( num : number ) {
+        let sum : number = 0;
+        let originNum : number = num;
+
+        while( num > 0 ) { //256 25 2, 0에서 반복문 나감
+            sum += num % 10; //0 + 6 6 + 5 13 + 0 sum = 13
+            num = Math.floor(num / 10); // num = 25 2 0
+        }
+        let answer : number = originNum + sum  //256 + 13 269
+        return answer;
+    }
+array1.push(solution(num));
+console.log(array2)
 }
-console.log(solution(256));
