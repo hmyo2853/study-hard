3a269c6350c0192c380031a010c73b457aa84707
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-06 15:39:58 +0900

    Update 220106_selfNumber.js

--- script/220106_selfNumber.js
+++ script/220106_selfNumber.js
@@ -1,7 +1,7 @@
 /* 각 자릿수 합 = 생성자가 있는 양수를 구하는 함수 */
 function solution( num : number ) : number {
   let sum : number = 0;
-  let originNum = num;
+  let originNum : number = num;
 
   while(num > 0) { //256 25 2, 0에서 반복문 나감
     sum += num % 10; //0 + 6 6 + 5 13 + 0 sum = 13
