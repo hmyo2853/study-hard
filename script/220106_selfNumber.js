981188d0a35f9e75349c9a029addb0530b91d602
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-06 22:25:15 +0900

    Update 220106_selfNumber.js
    10000까지 증가한 num으로 만든 생성자를 가지는 양의수로 이루어진 배열 array2

--- script/220106_selfNumber.js
+++ script/220106_selfNumber.js
@@ -1,9 +1,9 @@
 /* 1. 각 자릿수 합 = 생성자가 있는 양수를 구하는 함수 */
-/* 2. array1에 push하여 생성자가 있는 양수의 배열을 만들기*/
+/* 2. array1에 push하여 생성자가 있는 양수의 배열을 만들기 */
 /* 3. 전체 array1에서 array2의 중복 빼기(셀프넘버 구하기) */
 let array2 : Array<number> = [];
 
-for (let num = 257; num < 261; ++num) { //257부터 261까지
+for (let num = 1; num < 10000; ++num) { //257부터 261까지
     function solution( num : number ) {
         let sum : number = 0;
         let originNum : number = num;
@@ -15,8 +15,10 @@ for (let num = 257; num < 261; ++num) { //257부터 261까지
         let answer : number = originNum + sum  //256 + 13 269
         return answer;
     }
-array1.push(solution(num));
-console.log(array2) // 배열값 하나씩 들어가는거 확인
+  array2.push(solution(num));
+  array2.sort(function(a,b) { // 오름차순 2,4, ...
+    return a - b;
+  });
 }
 
 console.log(array2) // 배열값 모두 들어간 것 
