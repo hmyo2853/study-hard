c137fdd9bf7845c2e4a1d3d0bac5b5f570cc3435
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-06 15:04:35 +0900

    Update 220106_selfNumber.ts

--- script/220106_selfNumber.ts
+++ script/220106_selfNumber.ts
@@ -1,2 +1,11 @@
-let i = 2;
-console.log(i);
+/* 각 자릿수 합 = 생성자가 있는 양수 을 구하는 함수 */
+function solution(num) {
+  let sum = 0;
+  
+  while(num > 0) {
+    sum += num%10;
+    num = Math.floor(num / 10);
+  }
+  return sum;
+}
+console.log(solution(256));
