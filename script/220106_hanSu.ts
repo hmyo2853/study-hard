12b500e7dfd94b45fee29339d058556693e9089b
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-01-09 00:43:23 +0900

    Update 220106_hanSu.ts

--- script/220106_hanSu.ts
+++ script/220106_hanSu.ts
@@ -1,25 +1,27 @@
 /*
-  초기값 설정 array1 = 1부터 예제 숫자 N까지 모든 자연수 배열
-  array2는 함수를 돌려 나온 한수로 이루어진 자연수 배열
   N은 입력 값
+  array1은 
 */
 let N : number = 100;
 let array1 : Array<number> = [];
 let array2 : Array<number> = [];
 
-let i : number = 1;
 let realNum : Array<number> = [];
-while(i < N + 1) { //i = 1~100
-  if ( /* 등차수열 조건 */ ) {
-    let splitNum: Array<string> = i.toString().split(''); //['1','2','3'] string Array
-    realNum = splitNum.map(Number); //[1,2,3] num Array
-    // 해당 한수를 array2에 추가
-    array2.push(i);
+
+for(let i = 1; i < N + 1; ++i ) { //i = 1~100
+  let splitNum: Array<string> = i.toString().split(''); //['1','2','3'] string Array
+  realNum = splitNum.map(Number); //[1,2,3] num Array
+  let idx: number = 0;
+  while ( idx < realNum.length ) { //realNum index 값이 realNum의 길이보다 작아질때까지 돌아감
+    realNum
+    array1.push(i);
+    idx++;
   }
-  ++i;
-  console.log(realNum);
+  if () { //array1의 각 배열 값이 같으면 i 값을 array2에 push
+
+  }
 }
 // array1에서 자연수를 가져와 한수 배열 array2 만들기
+console.log(array1);
 
-
-console.log(array2.length); // 배열의 갯수 구하기
+// console.log(array2.length); // 배열의 갯수 구하기
