8252db22c02048485bff943cdefb74102548c159
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-03-30 22:42:58 +0900

    Create app.js

---
+++ study-language/JS/1. 바닐라 JS로 크롬 앱 만들기/복습내용/20220330-preventDefault/app.js
@@ -0,0 +1,13 @@
+// 1. form과 input을 변수로 지정해준다.
+const formBox = document.querySelector("#inputAndButton");
+const inputBox = document.querySelector("#inputAndButton input")
+
+// 3. sendSubmit 함수에서 정보를 event를 받아 preventDefault를 동작시키고 입력된 값을 콘솔로 출력한다.
+function sendSubmit (event){
+    event.preventDefault();
+    const name = inputBox.value;
+    console.log(name)
+}
+
+// 2. "submit" 이벤트가 있을때 sendSubmit 함수로 정보를 전달하며 호출한다.
+formBox.addEventListener("submit", sendSubmit)
