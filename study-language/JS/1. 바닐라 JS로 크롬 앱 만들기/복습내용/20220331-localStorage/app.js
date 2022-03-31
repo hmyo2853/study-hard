fc408217c5e33112e33479b71d1184f45988f598
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-03-31 15:14:22 +0900

    Create app.js

---
+++ study-language/JS/1. 바닐라 JS로 크롬 앱 만들기/복습내용/20220331-localStorage/app.js
@@ -0,0 +1,7 @@
+// setItem 은 브라우저에 기본으로 key와 value를 저장한다.
+// console창 => application => Local Storage에 값을 할당
+localStorage.setItem("KEY", "VALUE")
+
+// getItem 은 브라우저에 기본으로 저장되어있는 값을 가져온다.
+localStorage.getItem("KEY")
+// string "VALUE" 출력
