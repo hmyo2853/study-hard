4a2d854e4f4a83bdb8674071bf6ed2aac7e8397b
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-04-02 14:56:08 +0900

    weather.js
    현재 유저의 위치를 받고, open API를 사용하여 해당 위치에 알맞는 값을 가져온다.

---
+++ study-language/JS/1. 바닐라 JS로 크롬 앱 만들기/js/weather.js
@@ -0,0 +1,21 @@
+// openweatherAPI 이용하여 KEY값 가져오기
+const API_KEY = "8ffcb0c98864d9036b50c092e7d6ac80";
+function onGeoOk(userPosition){
+    const lat = userPosition.coords.latitude; // 위도
+    const long = userPosition.coords.longitude; //경도
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+    // 자바스크립트가 직접 응답을 요구함 promise(fetch) 이후 then으로 받음
+    // json()은 API로 받은 모든 값들을 가져온다
+    fetch(url).then(response => response.json()).then(data => {
+        const cityContainer = document.querySelector("#weather span:first-child");
+        const weatherContainer = document.querySelector("#weather span:last-child");
+        cityContainer.innerHTML = data.name;
+        weatherContainer.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+    alert("Cant find your weather.")
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
+
+
