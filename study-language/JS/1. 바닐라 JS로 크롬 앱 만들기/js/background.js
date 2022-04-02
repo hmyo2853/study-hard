813e83d22ca04fd6159c5a6ce004133334f60222
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-04-02 14:55:05 +0900

    background.js
    배경에 랜덤으로 보여줄 이미지

---
+++ study-language/JS/1. 바닐라 JS로 크롬 앱 만들기/js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg"
+]
+
+const chooseImg = images[Math.floor(Math.random() * images.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${chooseImg}`
+
+document.body.appendChild(bgImg);
