9d0fff814dbdfd491cf1a8756fe929e1ab45de42
hmyo2853 <hmyo2853@gmail.com>
2022-08-23 17:20:48 +0900

    Update 0818.py

--- study-language/Python/프로그래머스 코딩테스트/우너숙제/0818.py
+++ study-language/Python/프로그래머스 코딩테스트/우너숙제/0818.py
@@ -1,8 +1,8 @@
 array = [
-[0, 0, 0],
-[0, 0, 0],
-[0, 0, 0]]
-check = [False, False, False] # 1 check
+[1, 0, 0, 0],
+[0, 1, 0, 0],
+[0, 0, 0, 1]]
+check = [False, False, False, False] # 1 check
 i = 0
 
 def solution(array, check, i):
@@ -11,9 +11,12 @@ def solution(array, check, i):
     if check[i] == False:
       arr[i] = 1
       check[i] = True
-      check[i-1] = False
-      print(arr, check, i, check[i], check[i-1])
+      # 함수 = 2차열 array에 접근하는 index는 0,1,2 / 1을 찍는 순서는 2,1,0 이 될수도 있다.
+      # for문은 초기화가 되므로 함수에 들어갔을때 초기화될 것을 생각해서 함수를 짜기
+      # 맨마지막 동작에서 index 오류가 없게끔하기
       solution(array, check, i+1)
+      arr[i] = 0
+      check[i] = False
       
 solution(array, check, i)
 
@@ -24,9 +27,11 @@ solution(array, check, i)
 [0, 0, 0],
 [0, 0, 0]]
 
-[[1, 0, 0],
-[0, 1, 0],
-[0, 0, 1]]
+[[1, 0, 0], T,F,F
+[0, 1, 0], F,T,F
+[0, 0, 1]] f,F,T
+error
+
 
 [[1, 0, 0],
 [0, 0, 1],
