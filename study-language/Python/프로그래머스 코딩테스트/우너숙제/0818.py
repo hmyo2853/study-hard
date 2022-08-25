284afb4aedd504580d5cbb4091f8baf365edfc9b
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-08-25 21:46:22 +0900

    재귀함수 이해 1

--- study-language/Python/프로그래머스 코딩테스트/우너숙제/0818.py
+++ study-language/Python/프로그래머스 코딩테스트/우너숙제/0818.py
@@ -1,39 +1,133 @@
-array = [
-[1, 0, 0, 0],
-[0, 1, 0, 0],
-[0, 0, 0, 1]]
-check = [False, False, False, False] # 1 check
+array = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+check = [False, False, False]
 i = 0
 
-def solution(array, check, i):
-  while i < len(check):
-    arr = array[i]
-    if check[i] == False:
-      arr[i] = 1
-      check[i] = True
-      # 함수 = 2차열 array에 접근하는 index는 0,1,2 / 1을 찍는 순서는 2,1,0 이 될수도 있다.
-      # for문은 초기화가 되므로 함수에 들어갔을때 초기화될 것을 생각해서 함수를 짜기
-      # 맨마지막 동작에서 index 오류가 없게끔하기
-      solution(array, check, i+1)
-      arr[i] = 0
-      check[i] = False
-      
-solution(array, check, i)
+# 함수 조건1. TF로 방문했는지 확인 / 조건2. F이면 1을 찍는다 / 조건3. 함수 끝날때 초기화
+
+def solution(i):
+    if i > len(array) - 1:          # i가 3일때 재귀 종료
+        print(array)
+        print("-"*36)
+        return
+    for n in range(len(check)):
+        if check[n] == False:       # false일때
+            array[i][n] = 1
+            check[n] = True
+            solution(i+1)
+            array[i][n] = 0         # 초기화
+            check[n] = False
+
+solution(i)
 
 '''
-2차원 배열의 값 0을 한개씩 찍는 재귀함수 만들어보기
+21 : 함수를 호출한다. 000 000 000 FFF i=0
 
-[[0, 0, 0],
-[0, 0, 0],
-[0, 0, 0]]
+9 : 함수를 실행한다. 000 000 000 FFF i=0. if 패스, for n=0, check[0] = F 이므로
+    array[0][0] 1로 변경, 100 000 000 TFF가 된다.
+    solution 함수 호출. [재귀 1] i 1증가, 함수 실행 멈춤.
+    
+9 : [재귀 1] 함수 실행한다. 100 000 000 TFF i=1. if 패스, for n=0, check[0] = T 통과
+    for n=1, check[1] = F 이므로 array[1][1] 1로 변경 T변경, 100 010 000 TTF가 된다.
+    solution 함수 호출. [재귀 2] i 1증가, 함수 실행 멈춤.
+    
+9 : [재귀 2] 함수 실행한다. 100 010 000 TTF i=2, if 패스, for n=0, check[0] = T 통과
+    for n=1, check[1] = T 통과 for n=2, check[2] = F 이므로 array[2][2] 1로 변경 T변경, 100 010 001 TTT가 된다.
+    solution 함수 호출. [재귀 3] i 1증가, 함수 실행 멈춤.
+    
+9 : [재귀 3] 함수 실행한다. 100 010 001 TTT i=3, if 실행, print. 함수 실행 종료
 
-[[1, 0, 0], T,F,F
-[0, 1, 0], F,T,F
-[0, 0, 1]] f,F,T
-error
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=2, array[2][2] 0 변경 F변경, 100 010 000 TTF for 종료
+    함수 실행 종료
 
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=1, array[1][1] 0 변경 F변경, 100 000 000 TFF
+    n증가 for i=1, n=2, check[2] = F이므로 array[1][2] 1로 변경 T변경, 100 001 000 TFT
+    solution 함수 호출. [재귀 2] i 1증가, 함수 실행 멈춤
 
-[[1, 0, 0],
-[0, 0, 1],
-[0, 1, 0]]
+9 : [재귀 2] 함수 실행한다. 100 001 000 TFT i=2, if 패스, for n=0, check[0] = T 통과
+    for n=1, check[1] = F 이므로 array[2][1] 1로 변경 T변경, 100 001 010 TTT
+    solution 함수 호출. [재귀 3] i 1증가, 함수 실행 멈춤.
+
+9 : [재귀 3] 함수 실행한ㄷ나. 100 001 010 TTT i=3, if 실행, print. 함수 실행 종료
+
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=1, array[2][1] 0 변경 F변경, 100 001 000 TFT
+    n증가 for i=2, n=2, check[2] = T 통과, 함수 실행 종료
+
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=2, array[1][2] 0 변경 F변경, 100 000 000 TFF for 종료
+    함수 실행 종료
+
+17 : 멈췄던 함수 다시 동작. i=0, n=0, array[0][0] 0 변경 F변경, 000 000 000 FFF
+    n증가 for i=0, n=1, array[0][1] 1 변경 T변경, 010 000 000 FTF
+    solution 함수 호출. [재귀 1] i 1증가, 함수 실행 멈춤.
+
+9 : [재귀 1] 함수 실행한다. 010 000 000 FTF i=1. if 패스, for n=0, check[0] = F이므로
+    array[1][0] 1변경 T변경, 010 100 000 TTF
+    solution 함수 호출. [재귀 2] i 1증가, 함수 실행 멈춤.
+
+9 : [재귀 2] 함수 실행한다. 010 100 000 TTF i=2. if 패스, for n=0, check[0] = T 통과
+    for n=1, check[1] = T 통과 for n=2, check[2] = F이므로 array[2][2] 1로 변경 T변경, 010 100 001 TTT
+    solution 함수 호출. [재귀 3] i 1증가, 함수 실행 멈춤.
+
+9 : [재귀 3] 함수 실행한다. 010 100 001 TTT i=3, if 실행, print. 함수 실행 종료
+
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=2, array[2][2] 0 변경 F변경, 010 100 000 TTF for 종료
+    함수 실행 종료
+
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=0, array[1][0] 0 변경 F변경, 010 000 000 FTF
+    n증가 for i=1, n=1, check[1] = T 통과 for i=1, n=2, check[2] = F이므로
+    array[1][2] 1로 변경 T변경, 010 001 000 FTT
+    solution 함수 호출. [재귀 2] i 1증가, 함수 실행 멈춤
+
+9 : [재귀 2] 함수 실행한다. 010 001 000 FTT i=2. if 패스, for n=0, check[0] = F이므로
+    array[2][0] 1변경 T변경, 010 001 100 TTT
+    solution 함수 호출. [재귀 3] i 증가, 함수 실행 멈춤.
+
+9 : [재귀 3] 함수 실행한다. 010 001 100 TTT i=3. if 실행, print. 함수 실행 종료
+
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=0, array[2][0] 0 변경 F변경, 010 001 000 FTT
+    n증가 for i=2, n=1, check[1] = T 통과 for i=2, n=2, check[2] = T 통과 for 종료
+    함수 실행 종료
+
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=2, array[1][2] 0 변경 F변경, 010 000 000 FTF for 종료
+    함수 실행 종료
+
+17 : 멈췄던 함수 다시 동작. i=0, n=1, array[0][1] 0 변경 F변경, 000 000 000 FFF
+    n증가 for i=0, n=2, check[2] = F 이므로 array[0][2] 1로 변경 T변경 001 000 000 FFT
+    solution 함수 호출. [재귀 1] i 1증가, 함수 실행 멈춤
+
+9 : [재귀 1] 함수 실행한다. 001 000 000 FFT i=1. if 패스, for n=0, check[0] = F이므로
+    array[1][0] 1로 변경 T변경, 001 100 000 TFT
+    solution 함수 호출. [재귀 2] i 1증가, 함수 실행 멈춤
+
+9 : [재귀 2] 함수 실행한다. 001 100 000 TFT i=2. if 패스, for n=0, check[0] = T 통과
+    for n=1, check[1] = F이므로 array[2][1] 1로 변경 T변경, 001 100 010 TTT
+    solution 함수 호출. [재귀 3] i 증가, 함수 실행 멈춤.
+
+9 : [재귀 3] 함수 실행한다. 001 100 010 TTT i=3. if 실행, print. 함수 실행 종료
+
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=1, array[2][1] 0변경 F변경, 001 100 000 TFT
+    n 증가 for i=2, n=2, check[2] = T 통과 for 종료
+    함수 실행 종료
+
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=0, array[1][0] 0변경 F변경, 001 000 000 FFT
+    n 증가 for i=1, n=1, check[1] = F이므로 array[1][1] 1로 변경 T변경, 001 010 000 FTT
+    solution 함수 호출. [재귀 2] i 증가, 함수 실행 멈춤.
+
+9 : [재귀 2] 함수 실행한다. 001 010 000 FTT i=2. if 패스, for n=0, check[0] = F이므로
+    array[2][0] 1변경 T변경 001 010 100 TTT
+    solution 함수 호출. [재귀 3] i 증가, 함수 실행 멈춤.
+
+9 : [재귀 3] 함수 실행한다. 001 010 100 TTT i=3. if 실행, print. 함수 실행 종료
+
+17 : 멈췄던 [재귀 2] 함수 다시 동작. i=2, n=0, array[2][0] 0번경 F변경, 001 010 000 FTT
+    n증가 for i=2, n=1, check[1] = T 통과 for i=2, n=2, check[2] = T 통과 for 종료
+    함수 실행 종료
+
+17 : 멈췄던 [재귀 1] 함수 다시 동작. i=1, n=1, array[1][1] 0변경 F변경, 001 000 000 FFT
+    n증가 for i=1, n=2, check[2] = T 통과 for 종료
+    함수 실행 종료
+
+17 : 멈췄던 함수 다시 동작. i=0, n=2, array[0][2] 0변경 F변경, 000 000 000 FFF for 종료
+    함수 실행 종료
 '''
