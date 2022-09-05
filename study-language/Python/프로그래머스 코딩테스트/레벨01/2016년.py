1b0968ad9409b3ec20b03c06970618dc29e95272
hmyo2853 <hmyo2853@gmail.com>
2022-09-05 22:47:41 +0900

    프로그래머스 01레벨 업데이트

--- study-language/Python/프로그래머스 코딩테스트/레벨01/2016년.py
+++ study-language/Python/프로그래머스 코딩테스트/레벨01/2016년.py
@@ -1,11 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12901
 
-def solution(price, money, count):
-    answer = 0
-    final = 0
-    for times in range(1, count+1):
-        final += price * times
-    if money - final < 0:
-        return abs(money - final)
-    return answer
+import datetime
 
+def solution(a, b):
+    week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    date = datetime.date(2016, a, b)
+    return week[date.weekday()]
+    '''
+    1월은 31, 2월 29, 3월 31, 4월 30, 5월 31, 6월 30, 7월 31, 8월 31, 9월 30, 10월 31, 11월 30, 12월 31
+    if a == 1 or a == 2:
+        days = (30*(a-1)) + (a-1) + b - 1
+    else: # 홀짝
+        days = (30*(a-1)) + (a-()) + b - 1
+    return week[days % 7]
+        
+    '''
