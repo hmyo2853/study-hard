7c926e3584a266e338cba7fd73c845b566c421e0
hmyo2853 <hmyo2853@gmail.com>
2022-09-09 19:53:12 +0900

    프로그래머스 레벨2 업데이트

---
+++ study-language/Python/프로그래머스 코딩테스트/레벨02/짝지어제거하기.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12973
+
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
