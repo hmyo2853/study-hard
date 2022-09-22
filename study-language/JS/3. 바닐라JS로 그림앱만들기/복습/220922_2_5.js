cf0c3dc1d1a5997c51523452af1cb3b50cd415cb
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-09-22 21:36:23 +0900

    노마드 js 강의 업데이트 및 복습파일

--- study-language/JS/3. 바닐라JS로 그림앱만들기/복습/220922_2_5.js
+++ study-language/JS/3. 바닐라JS로 그림앱만들기/복습/220922_2_5.js
@@ -1,20 +1,12 @@
-const saveBtn = document.getElementById("save-btn");
-const textInput = document.getElementById("text");
 const modeBtn = document.getElementById("mode-btn");
-const eraserBtn = document.getElementById("erase-btn");
 const colorP = document.getElementById("color");
-const fileInput = document.getElementById("file-input")
 const colorCheaps = Array.from(document.getElementsByClassName("color_option"));
 const lineWidth = document.getElementById("lineWidth");
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
-
-canvas.height = CANVAS_WIDTH;
-canvas.width = CANVAS_HEIGHT;
+const ctx = canvas.getContext("2d")
+canvas.height = 800;
+canvas.width = 800;
 ctx.lineWidth = lineWidth.value;
-ctx.lineCap = "round";
 
 let isPainting = false;
 let isFilling = false;
@@ -62,47 +54,9 @@ function onModeChange() {
 
 function setFillCanvas() {
   if (isFilling) {
-    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(0, 0, 800, 800);
   }
 }
-
-function onEraser() {
-  ctx.strokeStyle = "white";
-  isFilling = false;
-  modeBtn.innerText = "Fill";
-}
-
-function onFileChange(event) {
-  const file = event.target.files[0];
-  const url = URL.createObjectURL(file);
-  const image = new Image();
-  image.src = url;
-  image.onload = function() {
-    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    fileInput.value = null;
-  }
-}
-
-function onDoubleClick(event) {
-  const text = textInput.value;
-  if (text !== "") {
-    const saveLine = ctx.lineWidth;
-    ctx.lineWidth = 1;
-    ctx.font = "48px serif"
-    ctx.fillText(text, event.offsetX, event.offsetY);
-    ctx.lineWidth = saveLine;
-  }
-}
-
-function onSaveClick() {
-  const url = canvas.toDataURL();
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "myDrawing.png";
-  a.click();
-}
-
-canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mousedown", setFillCanvas);
@@ -115,6 +69,3 @@ colorP.addEventListener("change", onColorChange);
 colorCheaps.forEach((color) => color.addEventListener("click", onColorClick));
 
 modeBtn.addEventListener("click", onModeChange);
-eraserBtn.addEventListener("click", onEraser);
-fileInput.addEventListener("change", onFileChange);
-saveBtn.addEventListener("click", onSaveClick);
